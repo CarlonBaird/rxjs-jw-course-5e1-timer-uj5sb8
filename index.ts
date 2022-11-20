913c708589c07218fe4b755b653b8adc842fb9dd
CarlonBaird <carlyestrella@live.com>
2022-11-20 18:32:24 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
-import { timer } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 
 console.log('App started');
 
-timer(2000).subscribe({
-  next: (value) => console.log(value),
-  complete: () => console.log('Completed!'),
+// timer(2000).subscribe({
+//   next: (value) => console.log(value),
+//   complete: () => console.log('Completed!'),
+// });
+
+const timer$ = new Observable<number>((subscriber) => {
+  setTimeout(() => {
+    subscriber.next(0);
+    subscriber.complete();
+  }, 2000);
 });
