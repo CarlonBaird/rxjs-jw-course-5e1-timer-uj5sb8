5849fd603f492b3f4d2a65b34b7de9f6dc37d9df
CarlonBaird <carlyestrella@live.com>
2022-11-20 18:47:04 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -2,23 +2,32 @@ import { Observable, timer } from 'rxjs';
 
 console.log('App started');
 
-const subscription = timer(2000).subscribe({
+const subscription1 = timer(2000).subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Sub1:', 'Completed!'),
+});
+
+setTimeout(() => {
+  console.log('Sub1:', 'Unsubscribed');
+  subscription1.unsubscribe();
+}, 1000);
+
+const timer$ = new Observable<number>((subscriber) => {
+  const timeoutId = setTimeout(() => {
+    console.log('Timeout - subscription 2!');
+    subscriber.next(0);
+    subscriber.complete();
+  }, 2000);
+
+  return () => clearTimeout(timeoutId);
+});
+
+const subscription2 = timer$.subscribe({
   next: (value) => console.log(value),
   complete: () => console.log('Completed!'),
 });
 
 setTimeout(() => {
-  subscription.unsubscribe();
+  console.log('Sub2:', 'Unsubscribed');
+  subscription2.unsubscribe();
 }, 1000);
-
-// const timer$ = new Observable<number>((subscriber) => {
-//   setTimeout(() => {
-//     subscriber.next(0);
-//     subscriber.complete();
-//   }, 2000);
-// });
-
-// timer$.subscribe({
-//   next: (value) => console.log(value),
-//   complete: () => console.log('Completed!'),
-// });
